b8e769002763efb7b24c0c4d0a1a6a804c768f79
Aaron Schung <aschung212@gmail.com>
2026-03-28 01:15:37 -0700

    fix: theme dropdown overlays content instead of pushing layout

--- src/App.vue
+++ src/App.vue
@@ -12,24 +12,25 @@
     <div v-else class="appContainer">
       <!-- Top bar -->
       <div class="topBar">
-        <button class="topBarBtn" @click="themeOpen = !themeOpen" title="Change theme" :aria-expanded="themeOpen">
-          <span class="themeActiveDot" :style="{ background: THEMES.find(t => t.id === currentTheme)?.dot }"></span>
-          <span class="topBarBtnLabel">Theme</span>
-        </button>
+        <div class="topBarLeft">
+          <button class="topBarBtn" @click="themeOpen = !themeOpen" title="Change theme" :aria-expanded="themeOpen">
+            <span class="themeActiveDot" :style="{ background: THEMES.find(t => t.id === currentTheme)?.dot }"></span>
+            <span class="topBarBtnLabel">Theme</span>
+          </button>
+          <!-- Theme picker dropdown -->
+          <div v-if="themeOpen" class="themeDropdown">
+            <button
+              v-for="t in THEMES"
+              :key="t.id"
+              :class="['themeOption', { active: currentTheme === t.id }]"
+              @click="currentTheme = t.id; themeOpen = false"
+            >
+              <span class="themeSwatchDot" :style="{ background: t.dot }"></span>
+              {{ t.label }}
+            </button>
+          </div>
+        </div>
         <button class="topBarBtn" @click="signOut">Sign out</button>
-      </div>
-
-      <!-- Theme picker dropdown -->
-      <div v-if="themeOpen" class="themeDropdown">
-        <button
-          v-for="t in THEMES"
-          :key="t.id"
-          :class="['themeOption', { active: currentTheme === t.id }]"
-          @click="currentTheme = t.id; themeOpen = false"
-        >
-          <span class="themeSwatchDot" :style="{ background: t.dot }"></span>
-          {{ t.label }}
-        </button>
       </div>
 
       <WorkoutTracker />
